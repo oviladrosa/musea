694a3cdc1dab27d1f0317fa9312fcc8508eaa2c9
aniol-carbo <aniol.carbo@estudiantat.upc.edu>
2021-03-09 22:38:47 +0100

    add html to test deploy

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
-var express =  require('express');
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').load()
+}
 
-var app = express();
+const express = require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
+
+const indexRouter = require('./routes/index')
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
 const db = mongoose.connection
 db.on('error', error => console.error(error))
-db.once('open', () => console.log('Connected to Mongoose'));
+db.once('open', () => console.log('Connected to Mongoose'))
 
-app.get("/", function(req,res){
-    res.send("Welcome to Musea!")
-});
+app.use('/', indexRouter)
 
-var server = app.listen(3000, function(){
-    var host = server.address().address;
-    var port = server.address().port;
-
-    console.log("Server is listening at port: ",port);
-});
+app.listen(process.env.PORT || 3000)
